3d6ed0f281e55abd1fe28209381320ec41ce291f
MuhsenOmari <muhsenomari44@gmail.com>
2025-04-24 11:16:43 +0430

    Login Serializer Added

--- Backend/settings.py
+++ Backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'core',
     'userauths',
     'api',
+    'rest_framework',
     
 ]
 
--- api/serializer.py
+++ api/serializer.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 from userauths.models import User, Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['full_name'] = user.full_name
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
 
 class UserSerializer(serializers.ModelSerializer):
 
